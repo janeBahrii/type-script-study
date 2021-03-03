644d611dfb237268e2b94a9350ba89ab3c9413c3
Jane Bahrii <jane@Janes-MacBook-Air.local>
2021-03-03 16:23:15 +0200

    adding .gitignore

--- using-ts.js
+++ using-ts.js
@@ -2,6 +2,9 @@ var button = document.querySelector("button");
 var input1 = document.getElementById("num1");
 var input2 = document.getElementById("num2");
 function add(num1, num2) {
+    console.log("sum");
+    console.log("made a change");
+    console.log("testing .gitignore");
     return num1 + num2;
 }
 button.addEventListener("click", function () {
