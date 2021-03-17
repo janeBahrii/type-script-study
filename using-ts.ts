e00876f3278e507f972269adbfc9dafc5332152f
Yevheniia Bahrii <Jnezhil@gmail.com>
2021-03-17 14:03:30 +0200

    initual state

--- using-ts.ts
+++ using-ts.ts
@@ -3,8 +3,6 @@ const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
 
 function add(num1: number, num2: number) {
-    console.log("sum");
-    console.log("made a change");
   return num1 + num2;
 }
 
